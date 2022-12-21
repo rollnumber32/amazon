43a0c398902b9a27457bf3f1889e4bd673702949
Lakshay Mittal <20bca1209@cuchd.in>
2022-12-21 14:19:55 +0530

    Home page complete

--- src/Home.js
+++ src/Home.js
@@ -16,13 +16,43 @@ function Home() {
                 <div className="product-row">
                     <Product
                         title="MacBook Air"
+                        price="92900.00"
+                        stars="5"
                         src="./p1.jpg"
                         alt="macbookair"
                         route="./ma"
                     />
                     <Product
                         title="Logitech Mechanical Keyboard"
+                        price="5999.00"
+                        stars="5"
                         src="./p2.jpg"
+                        alt="logitechmechanicalkeyboard"
+                        route="./lma"
+                    />
+                </div>
+                <div className="product-row">
+                    <Product
+                        title="Rubber Plant for Decoration"
+                        price="739.00"
+                        stars="3"
+                        src="./p3.jpg"
+                        alt="macbookair"
+                        route="./ma"
+                    />
+                    <Product
+                        title="Shure MV7 USB Mic"
+                        price="25600.00"
+                        stars="4"
+                        src="./p4.jpg"
+                        alt="logitechmechanicalkeyboard"
+                        route="./lma"
+                    />
+                    <Product
+                        title="ARCTIC COSMOS Acoustic Drum Kit"
+                        price="20890.00"
+                        stars="4"
+                        src="./p5.jpg"
                         alt="logitechmechanicalkeyboard"
                         route="./lma"
                     />
--- src/Product.js
+++ src/Product.js
@@ -1,18 +1,28 @@
 import "./Product.css";
+import { StarRate } from "@material-ui/icons";
 
 function Product(props) {
+    const stars = [];
+    for (let i = 0; i < parseInt(props.stars); i++) {
+        stars.push(<StarRate style={{ color: "#fbd432" }} />);
+    }
     return (
         <div className="product">
-            <div>{props.title}</div>
+            <div className="product__title">{props.title}</div>
+            <div>
+                ₹<span className="product__price">{props.price}</span>
+            </div>
+            <div>{stars}</div>
             <div
                 style={{
                     display: "flex",
                     flexDirection: "column",
-                    alignItems: "middle",
+                    alignItems: "center",
+                    gap: "32px",
                 }}
             >
                 <img className="product__img" src={props.src} alt={props.alt} />
-                <a href={props.route}>Add to Basket</a>
+                <button className="product__btn">Add to Basket</button>
             </div>
         </div>
     );
